359dda70bcb81cf00aba7b4f6af640e3905393fb
Allison <allisonmariebrowne@gmail.com>
2015-02-09 18:31:28 -0500

    shoulda model tests for tasks complete

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  let(:blanks) { ["", nil] }
+
+  it { should have_valid(:title)/when('a title') }
+  it { should_not have_valid(:title).when(*blanks) }
+
+  it { should have_valid(:user).when(User.new) }
+  it { should_not have_valid(:user).when(nil) }
+end
