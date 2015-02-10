3b26c3a09b544b55b58e77b85891e9967a6c0e86
Allison <allisonmariebrowne@gmail.com>
2015-02-09 19:40:20 -0500

    removes devise, adds user unit tests for user

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ActiveRecord::Base
+  validates_presence_of :title
+  validates_presence_of :user
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:blanks) { ['', nil] }
+
+  it { should have_valid(:first_name).when('Johnny') }
+  it { should_not have_valid(:first_name).when(*blanks) }
+
+  it { should have_valid(:last_name).when('Smith') }
+  it { should_not have_valid(:last_name).when(*blanks) }
 end
